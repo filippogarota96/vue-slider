937edf8f9b9b30e7227035c5f5a29990cee3a888
Filippo Garota <filogarota101@gmail.com>
2021-03-25 15:50:28 +0100

    Array delle immagini con vue

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    images: [
+    'img/barcellona.jpg',
+    'img/urbino.jpg',
+    'img/edimburgo.jpg',
+    'img/bruges.jpg',
+  ],
+  methods: {
+    
+  }
+}
+});
