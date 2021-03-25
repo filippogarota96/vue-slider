83d3d7985f91074adf273e5ea32e057dc2526166
Filippo Garota <filogarota101@gmail.com>
2021-03-25 16:21:56 +0100

    Evento click sulle immagini

--- js/script.js
+++ js/script.js
@@ -7,8 +7,20 @@ var app = new Vue ({
     'img/edimburgo.jpg',
     'img/bruges.jpg',
   ],
+  imgIndex : 0,
+  },
   methods: {
-    
+    nextImage: function (){
+      this.imgIndex ++;
+      if (this.imgIndex == this.images.length) {
+          this.imgIndex = 0;
+      }
+    },
+    prevImage: function (){
+      this.imgIndex --
+      if (this.imgIndex < 0 ) {
+          this.imgIndex = this.images.length - 1;
+      }
+    },
   }
-}
 });
